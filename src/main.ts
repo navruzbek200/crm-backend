e468f175c9d701e6ff209c947e56aab9610a5a95
Navruzbek Bektemirov <157490030+navruzbek200@users.noreply.github.com>
2025-06-03 10:34:56 +0500

    I changed backend files

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,10 @@ async function bootstrap() {
 
   // CORS configuration
   app.enableCors({
-    origin: "*"
-  })
-    
+  origin: "http://localhost:5173",
+  credentials: true,
+});
+
   // API prefix
   app.setGlobalPrefix("api")
 
